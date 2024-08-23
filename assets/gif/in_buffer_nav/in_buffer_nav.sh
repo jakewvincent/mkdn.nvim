d070f969c645d26314b6dc9446700b1172eed6ce
jakewvincent <jakewvincent@gmail.com>
2024-08-23 07:06:21 -0700

    chore: Organize gifs & associated files by folder

---
+++ assets/gif/in_buffer_nav/in_buffer_nav.sh
@@ -0,0 +1,2 @@
+agg --theme 222436,c8d3f5,444a73,ff757f,c3e88d,ffc777,82aaff,c099ff,86e1fc,c8d3f5 --font-family "MonoLisaCustom Nerd Font" in_buffer_nav.cast in_buffer_nav.gif
+magick in_buffer_nav.gif -coalesce -delete 0-24,44--1 in_buffer_nav.gif
